888a4fa81ff6bf4dd304dce5a68181b896d557e3
BleedAngel <aa0918016615@gmail.com>
2026-06-03 09:41:58 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# 初始化EV3主機
+ev3 = EV3Brick()
+ev3.speaker.beep()
+
+# 
+'''
+Port S1: 觸碰感應器
+Port S4: 顏色感應器
+Port D: 左後腿馬達
+Port A: 右後腿馬達
+Port B: 頭部馬達
+'''
+
+# 初始化後腿馬達+站立角度
+left_leg_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+right_leg_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+HALF_UP_ANGLE = 25
+STAND_UP_ANGLE = 65
+
+def 
